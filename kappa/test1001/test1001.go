fc621761ee2ba8110a4fd1430b276eead86f75b0
kappa <kimgeonkuk@gmail.com>
2022-07-08 16:14:12 +0900

    [ADD] test1001

---
+++ kappa/test1001/test1001.go
@@ -0,0 +1,24 @@
+package test1001
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Test1001() {
+	br := bufio.NewReader(os.Stdin)
+	str, _ := br.ReadString(('\n'))
+
+	blank := strings.Fields(str)
+
+	numStr1 := blank[0]
+	numStr2 := blank[1]
+
+	num1, _ := strconv.Atoi(numStr1)
+	num2, _ := strconv.Atoi(numStr2)
+
+	fmt.Println(num1 - num2)
+}
