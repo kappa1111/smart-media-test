f5552f825dac7a3e8692c9ea572978f4f894f346
kappa <kimgeonkuk@gmail.com>
2022-07-08 14:40:09 +0900

    [ADD] test10718

--- kappa/main.go
+++ kappa/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"kappa/test2557"
-)
-
 func main() {
-	// names := []string{"홍길동", "이순신", "강감찬"}
-
-	// for i, v := range names {
-	// 	fmt.Println(i)
-	// 	fmt.Println(v)
-
-	// }
-	test2557.Test2557()
-}
-
-func Test2557() {
 
 }
---
+++ kappa/test10718/test10718.go
@@ -0,0 +1,8 @@
+package test10718
+
+import "fmt"
+
+func Test10718() {
+	fmt.Println("강한친구 대한육군")
+	fmt.Println("강한친구 대한육군")
+}
