396cca24a797dfd91b04a740d40973562adaac95
kappa <kimgeonkuk@gmail.com>
2022-07-11 11:10:07 +0900

    [ADD] test25083

---
+++ kappa/test25083/test25083.go
@@ -0,0 +1,14 @@
+package test25083
+
+import "fmt"
+
+func Test25083() {
+
+	fmt.Println("         ,r'\"7")
+	fmt.Println("r`-_   ,'  ,/")
+	fmt.Println(" \\. \". L_r'")
+	fmt.Println("   `~\\/")
+	fmt.Println("      |")
+	fmt.Println("      |")
+
+}
