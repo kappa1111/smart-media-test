e963a4839c73260045d8960a9168a799c3649e04
kappa <kimgeonkuk@gmail.com>
2022-07-11 09:59:13 +0900

    [ADD] tets1008

---
+++ kappa/test1008/test1008.go
@@ -0,0 +1,24 @@
+package test1008
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Test1008() {
+	br := bufio.NewReader(os.Stdin)
+	str, _ := br.ReadString(('\n'))
+
+	blank := strings.Fields(str)
+
+	numStr1 := blank[0]
+	numStr2 := blank[1]
+
+	num1, _ := strconv.Atoi(numStr1)
+	num2, _ := strconv.Atoi(numStr2)
+
+	fmt.Println(float64(num1) / float64(num2))
+}
