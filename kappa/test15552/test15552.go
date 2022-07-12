1244e50e1102cac7da71cfdb177859a9ac306724
kappa <kimgeonkuk@gmail.com>
2022-07-12 11:41:23 +0900

    [ADD] test15552

---
+++ kappa/test15552/test15552.go
@@ -0,0 +1,22 @@
+package test15552
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func Test15552() {
+	defer writer.Flush()
+
+	var T int
+	fmt.Fscan(reader, &T)
+	for i := 0; i < T; i++ {
+		var A, B int
+		fmt.Fscan(reader, &A, &B)
+		fmt.Fprintln(writer, A+B)
+	}
+}
